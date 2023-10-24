063b880fba20b086beb3b18d9eadf50c77763e75
Emilio Davis <emilio.davis@khipu.com>
2023-10-24 14:00:47 -0300

    Initial commit

---
+++ Example/KhenshinSecureMessage/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  KhenshinSecureMessage
+//
+//  Created by Emilio Davis on 10/24/2023.
+//  Copyright (c) 2023 Emilio Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
